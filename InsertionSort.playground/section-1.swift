51972dd69d9fe583c883a9fc327f99be6f05e522
Bruno Capezzali <capezzbr@gmail.com>
2014-07-14 00:20:19 +0100

    Implementation done

--- InsertionSort.playground/section-1.swift
+++ InsertionSort.playground/section-1.swift
@@ -2,24 +2,19 @@
 
 import Cocoa
 
-func swap(var num1 : Int, var with num2 : Int) {
-    var tmp = num1
-    num1 = num2
-    num2 = tmp
-}
-
 var data = [10, 3, 2, 12, 5, 6]
-swap(data[10], with: data[6])
-data
 
-
-
- 
-func sort(toSort : Int[]) {
-    var current = toSort[0];
-    for var i = 1; i<toSort.count; i++ {
-        while current > toSort[i] && i < toSort.count {
-            
+func insertionSort<T : Comparable>(toSort : T[]) {
+    for i in 1..toSort.count {
+        var value = toSort[i]
+        var j = i-1
+        while j >= 0 && toSort[j] > value {
+            toSort[j+1]  = toSort[j]
+            j--
         }
+        toSort[j+1]  = value
     }
 }
+
+insertionSort(data)
+data
