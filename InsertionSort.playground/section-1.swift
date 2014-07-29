37758c4642c35eb7b5e844919c99f9e016ad1bd4
Bruno Capezzali <capezzbr@gmail.com>
2014-07-29 23:47:53 +0100

    Updated InsertionSort.playground to Xcode 6 beta 4

--- InsertionSort.playground/section-1.swift
+++ InsertionSort.playground/section-1.swift
@@ -4,15 +4,15 @@ import Cocoa
 
 var data = [10, 3, 2, 12, 5, 6]
 
-func insertionSort<T : Comparable>(toSort : T[]) {
-    for i in 1..toSort.count {
+func insertionSort<T : Comparable>(toSort : [T]) {
+    for i in 1 ..< toSort.count {
         var value = toSort[i]
-        var j = i-1
+        var j = --i
         while j >= 0 && toSort[j] > value {
-            toSort[j+1]  = toSort[j]
-            j--
+            toSort[++j] = toSort[j]
+            --j
         }
-        toSort[j+1]  = value
+        toSort[++j] = value
     }
 }
 
