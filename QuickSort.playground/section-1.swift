ac906334d614fdc6e6eb7308ee78c35eff7695c6
Bruno Capezzali <capezzbr@gmail.com>
2014-08-01 23:58:19 +0100

    Moved the algorithm inside a class

--- QuickSort.playground/section-1.swift
+++ QuickSort.playground/section-1.swift
@@ -2,49 +2,67 @@
 
 import Cocoa
 
-//func quicksort<T : Comparable>(data : [T]) {
-//    if data.count == 0 {
-//        return
-//    }
-//    
-//    self.data = data
-//    quickSort(0, toIndex: values.count - 1)
-//}
-
-func quickSort<T : Comparable>(inout data : [T], low : Int, high : Int) {
-    var i = low
-    var j = high
-    let pivot = data[low + (high - low) / 2]
+class QuickSort<T : Comparable> {
     
-    while i <= j {
-        while data[i] < pivot {
-            ++i
-        }
-        
-        while data[j] > pivot {
-            --j
-        }
-        
-        if i <= j {
-            swapItemAtIndex(&data, i, j)
-            ++i
-            --j
-        }
+    private var data : [T] {
+    get {
+        return self.data
+    }
+    set(newData) {
+        self.data = newData
+    }
     }
     
-    if low < j {
-        quickSort(&data, low, j)
+    public init() {
+        self.data = []
     }
-    if i < high {
-        quickSort(&data, i, high)
+    
+    public func sort() {
+        if data.count == 0 {
+            return
+        }
+        
+        quickSort(0, high: data.count - 1)
     }
-}
-
-func swapItemAtIndex<T : Comparable>(inout data : [T], index : Int, withElementAtIndex : Int) {
-    let temp = data[index];
-    data[index] = data[withElementAtIndex];
-    data[withElementAtIndex] = temp;
+    
+    private func quickSort(low : Int, high : Int) {
+        var i = low
+        var j = high
+        let pivot = data[low + (high - low) / 2]
+        
+        while i <= j {
+            while data[i] < pivot {
+                ++i
+            }
+            while data[j] > pivot {
+                --j
+            }
+            if i <= j {
+                swapItemAtIndex(i, withElementAtIndex: j)
+                ++i
+                --j
+            }
+        }
+        
+        if low < j {
+            quickSort(low, high: j)
+        }
+        if i < high {
+            quickSort(i, high: high)
+        }
+    }
+    
+    func swapItemAtIndex(index : Int, withElementAtIndex : Int) {
+        let temp = data[index];
+        data[index] = data[withElementAtIndex];
+        data[withElementAtIndex] = temp;
+    }
 }
 
 var data = [10, 3, 2, 12, 5, 6]
-quickSort(&data, 0, data.count-1)
+let quickSort = QuickSort<Int>()
+quickSort.sort()
+var aa = quickSort.data
+
+
+
