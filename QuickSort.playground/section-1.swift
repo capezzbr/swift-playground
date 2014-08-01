9fa1f7c158de605525161b2f71bae0fe417d0e4a
Bruno Capezzali <capezzbr@gmail.com>
2014-08-02 00:15:58 +0100

    Working QuickSort using class

--- QuickSort.playground/section-1.swift
+++ QuickSort.playground/section-1.swift
@@ -4,14 +4,7 @@ import Cocoa
 
 class QuickSort<T : Comparable> {
     
-    private var data : [T] {
-    get {
-        return self.data
-    }
-    set(newData) {
-        self.data = newData
-    }
-    }
+    public var data : [T]
     
     public init() {
         self.data = []
@@ -21,7 +14,6 @@ class QuickSort<T : Comparable> {
         if data.count == 0 {
             return
         }
-        
         quickSort(0, high: data.count - 1)
     }
     
@@ -52,17 +44,19 @@ class QuickSort<T : Comparable> {
         }
     }
     
-    func swapItemAtIndex(index : Int, withElementAtIndex : Int) {
+    private func swapItemAtIndex(index : Int, withElementAtIndex : Int) {
         let temp = data[index];
         data[index] = data[withElementAtIndex];
         data[withElementAtIndex] = temp;
     }
 }
 
-var data = [10, 3, 2, 12, 5, 6]
-let quickSort = QuickSort<Int>()
+var quickSort = QuickSort<Int>()
+quickSort.data = [10, 3, 2, 12, 5, 6]
 quickSort.sort()
-var aa = quickSort.data
+quickSort.data
+
+
 
 
 
